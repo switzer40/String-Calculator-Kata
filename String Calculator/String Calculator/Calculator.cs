713af9ca8966d28c9b9b4427b585e8e89e618d8f
switzer40 <switzer40@googlemail.com>
2017-02-07 04:40:04 +0100

    First test psses

---
+++ String Calculator/String Calculator/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace String_Calculator
+{
+    internal class Calculator
+    {
+        public Calculator()
+        {
+        }
+
+        internal int Add(string numbers)
+        {
+            return 0;
+        }
+    }
+}
